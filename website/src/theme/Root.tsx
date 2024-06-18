7b779afd7d47ee83dd1a3bad8c819a4e7f9811c1
Viki <hi@viki.moe>
2024-06-18 15:39:02 +0800

    docs: set favicon

--- website/src/theme/Root.tsx
+++ website/src/theme/Root.tsx
@@ -1,6 +1,12 @@
 import 'uno.css'
+
 import React from 'react'
+import { useFavicon, useMount } from '@site/../src'
 
 export default function Root({ children }) {
+  const [_, actions] = useFavicon()
+
+  useMount(() => actions.setEmojiFavicon('🪝'))
+
   return <>{children}</>
 }
