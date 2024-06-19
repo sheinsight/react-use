45cf6c8987d5d7d6ef9addd1f9374c0709c5a8d5
Viki <hi@viki.moe>
2024-06-19 11:19:08 +0800

    docs: modify date format

--- src/use-date-format/demo.tsx
+++ src/use-date-format/demo.tsx
@@ -3,37 +3,34 @@ import { useControlledComponent, useDateFormat, useNow } from '@shined/react-use
 
 export function App() {
   const now = useNow()
-  const dateTime = useDateFormat(now, 'YYYY-MM-DD ddd HH:mm:ss.SSS')
-  const unicodeDateTime = useDateFormat(now, 'yyyy-MM-dd eee HH:mm:ss.SSS', {
-    unicodeSymbols: true,
-  })
 
-  const input = useControlledComponent('[Now is] YYYY-MM-DD ddd hh:mm:ss AA')
+  const dateTime = useDateFormat(now, 'YYYY-MM-DD dddd HH:mm:ss.SSS')
+  const input = useControlledComponent('[Now is] YYYY-MM-DD dddd hh:mm:ss AA')
   const playground = useDateFormat(now, input.value)
 
-  const unicodeInput = useControlledComponent('[Now is] yyyy-MM-dd eee HH:mm:ss aaaa')
+  const unicodeDateTime = useDateFormat(now, 'yyyy-MM-dd eeee HH:mm:ss.SSS', { unicodeSymbols: true })
+  const unicodeInput = useControlledComponent('[Now is] yyyy-MM-dd eeee HH:mm:ss aaaa')
   const unicodePlayground = useDateFormat(now, unicodeInput.value, { unicodeSymbols: true })
 
   return (
     <Card>
       <h3 className="mb-0">Convention Date Symbols (by default)</h3>
       <Zone border="primary" row={false}>
-        <KeyValue labelWidth="60px" label="Format" value="YYYY-MM-DD ddd HH:mm:ss.SSS" />
-        <KeyValue labelWidth="60px" label="Result" value={dateTime} />
+        <KeyValue label="Format" value="YYYY-MM-DD ddd HH:mm:ss.SSS" />
+        <KeyValue label="Result" value={dateTime} />
       </Zone>
       <Zone border="primary" row={false}>
-        <KeyValue labelWidth="80px" label="Result" value={playground} />
-        <LabelInput labelWidth="80px" label="Template" className="flex-1" placeholder="Template" {...input.props} />
+        <KeyValue label="Result" value={playground} />
+        <LabelInput label="Template" className="flex-1" {...input.props} />
       </Zone>
       <h3 className="mb-0">Unicode Standard Date Symbols</h3>
       <Zone border="primary" row={false}>
-        <KeyValue labelWidth="60px" label="Format" value="yyyy-MM-dd eee HH:mm:ss.SSS" />
-        <KeyValue labelWidth="60px" label="Result" value={unicodeDateTime} />
+        <KeyValue label="Format" value="yyyy-MM-dd eee HH:mm:ss.SSS" />
+        <KeyValue label="Result" value={unicodeDateTime} />
       </Zone>
       <Zone border="primary" row={false}>
-        <KeyValue labelWidth="80px" label="Result" value={unicodePlayground} />
-        {/* biome-ignore format: no wrap */}
-        <LabelInput labelWidth="80px" label="Template" className="flex-1" placeholder="Template" {...unicodeInput.props} />
+        <KeyValue label="Result" value={unicodePlayground} />
+        <LabelInput label="Template" className="flex-1" {...unicodeInput.props} />
       </Zone>
     </Card>
   )
