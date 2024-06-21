43d961ad7b38ea83133db3d5bc7b591ec38ce017
Viki <hi@viki.moe>
2024-06-21 15:09:04 +0800

    docs(useTitle): refine Hook API

--- scripts/generate-dependencies-graph.ts
+++
@@ -1,9 +0,0 @@
-// https://github.com/pahen/madge
-
-// 1. pnpm --global add madge
-// (macOS) 2. brew install graphviz
-// (Ubuntu) 2. apt-get install graphviz
-
-// madge --image graph.png --exclude '((index\.mdx)|(demo\.tsx))'  ./src/**/*
-
-console.log('done!')
