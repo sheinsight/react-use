a5117f4ff6521822147b080748ae2c69e3c81e69
Viki <hi@viki.moe>
2024-07-03 16:55:10 +0800

    docs: fix title error & modify demo

--- src/use-target-element/demo.tsx
+++ src/use-target-element/demo.tsx
@@ -1,27 +1,39 @@
 import { Card } from '@/components'
 import { useIntervalFn, useTargetElement } from '@shined/react-use'
+import { useId, useRef } from 'react'
 
 export function App() {
-  const el1 = useTargetElement('button.el')
-  const el2 = useTargetElement('#id')
-  const el3 = useTargetElement('div.class')
-  // const el4 = useTargetElement(document.body) // not SSR-friendly
-  const el5 = useTargetElement(() => window)
-  const el7 = useTargetElement(() => document.documentElement)
-  const el6 = useTargetElement(() => document.querySelector('section'))
+  const ref = useRef<HTMLDivElement | null>(null)
+  const el1 = useTargetElement(ref) // Recommended, specific element, SSR-friendly
+
+  const id = useId() // Recommended, if you prefer to target element by id
+  const el2 = useTargetElement(`#${CSS.escape(id)}-div`)
+
+  const el3 = useTargetElement(() => window)
+  const el4 = useTargetElement(() => document.documentElement)
+  const el5 = useTargetElement(() => document.querySelector('section'))
+  const el6 = useTargetElement('button.green-btn')
+  const el7 = useTargetElement('#el-id')
+  const el8 = useTargetElement('div.blue-bg')
+  const el9 = useTargetElement(document.body) // NOT recommended, it's not SSR-friendly
 
   useIntervalFn(() => {
-    console.log(el1.current, el2.current, el3.current, el5.current, el6.current, el7.current)
+    // biome-ignore format: no wrap
+    console.log([el1.current, el2.current, el3.current, el4.current, el5.current, el6.current, el7.current, el8.current, el9.current])
   }, 1000)
 
   return (
     <Card row>
-      <button type="button" className="el border-0 text-base rounded bg-primary/80 px-4 py-1">
-        button.el
+      <button type="button" className="green-btn border-0 text-base rounded bg-primary/60 px-4 py-1">
+        button.green-btn
       </button>
-      {/* biome-ignore format: for demo */}
-      <div className="rounded bg-amber/80 px-4 py-1" id="id">#id</div>
-      <div className="class rounded bg-blue/80 px-4 py-1">div.class</div>
+      {/* biome-ignore format: no wrap */}
+      <div id="el-id" className="rounded bg-amber/80 px-4 py-1">#el-id</div>
+      {/* biome-ignore format: no wrap */}
+      <div ref={ref} className="rounded bg-cyan/80 px-4 py-1">ref</div>
+      <div className="blue-bg rounded bg-blue/80 px-4 py-1">div.class</div>
+      {/* biome-ignore format: no wrap */}
+      <div id={`${id}-div`} className="blue-bg rounded bg-purple/80 px-4 py-1">{'id={`${id}-div`}'}</div>
       <section className="rounded bg-red/80 px-4 py-1">section</section>
     </Card>
   )
