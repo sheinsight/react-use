55f57bfe87f58e311e9f8b9ebc24c38ac6b453ec
Viki <hi@viki.moe>
2024-07-10 10:26:49 +0800

    docs: modify demo

--- src/use-layout-mount/demo.tsx
+++ src/use-layout-mount/demo.tsx
@@ -1,10 +1,10 @@
-import { Button, Card, cn } from '@/components'
-import { useLayoutMount, useMount, useToggle } from '@shined/react-use'
+import { Card } from '@/components'
+import { useLayoutMount, useMount } from '@shined/react-use'
+import { useRef } from 'react'
 
 export function App() {
   return (
     <Card>
-      <Button onClick={() => window.location.reload()}>Refresh page</Button>
       <LayoutMount />
       <Mount />
     </Card>
@@ -12,15 +12,31 @@ export function App() {
 }
 
 function Mount() {
-  const [status, toggle] = useToggle(true)
-  const cls = status ? 'bg-red/80 w-60 h-20' : 'bg-gray/80 w-54 h-12'
-  useMount(toggle)
-  return <div className={cn('p-4 h-12 text-white rounded flex items-center', cls)}>useMount (layout jitter)</div>
+  const ref = useRef<HTMLDivElement>(null)
+
+  useMount(() => {
+    const { height } = ref.current?.getBoundingClientRect() ?? {}
+    console.log('useMount', height)
+  })
+
+  return (
+    <div ref={ref} className={'p-4 bg-primary/80 text-white rounded flex items-center'}>
+      useMount
+    </div>
+  )
 }
 
 function LayoutMount() {
-  const [status, toggle] = useToggle(true)
-  const cls = status ? 'bg-red/80 w-60 h-20' : 'bg-gray/80 w-54 h-12'
-  useLayoutMount(toggle)
-  return <div className={cn('p-4 text-white rounded flex items-center', cls)}>useLayoutMount</div>
+  const ref = useRef<HTMLDivElement>(null)
+
+  useLayoutMount(() => {
+    const { height } = ref.current?.getBoundingClientRect() ?? {}
+    console.log('useLayoutMount', height)
+  })
+
+  return (
+    <div ref={ref} className={'p-4 bg-primary/80 text-white rounded flex items-center'}>
+      useLayoutMount
+    </div>
+  )
 }
