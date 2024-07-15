5e4f003aaf4dd2bcd37402a1e24e816da2b48043
Viki <hi@viki.moe>
2024-07-15 14:30:54 +0800

    feat: new hook `useBoolean`

--- src/use-boolean/demo.tsx
+++ src/use-boolean/demo.tsx
@@ -1,20 +1,20 @@
 import { Button, Card, KeyValue, Zone } from '@/components'
-import { useCircularList, useToggle } from '@shined/react-use'
+import { useBoolean, useCircularList } from '@shined/react-use'
 
 export function App() {
-  const [mode, toggleMode, actions] = useToggle(['light', 'dark'] as const)
+  const [bool, actions] = useBoolean(true)
   const [value, listActions] = useCircularList(['A', 'B', 'C', 'D', 'F', 'G'])
 
   return (
     <Card>
       <Zone>
-        <KeyValue label="mode" value={mode} />
+        <KeyValue label="bool" value={bool} />
         <KeyValue label="value" value={value} />
       </Zone>
       <Zone>
-        <Button onClick={toggleMode}>Toggle mode</Button>
-        <Button onClick={actions.setLeft}>Set left</Button>
-        <Button onClick={actions.setRight}>Set right</Button>
+        <Button onClick={actions.toggle}>Toggle bool</Button>
+        <Button onClick={actions.setTrue}>Set true</Button>
+        <Button onClick={actions.setFalse}>Set false</Button>
         <Button onClick={() => listActions.prev()}>Prev value</Button>
         <Button onClick={() => listActions.next()}>Next value</Button>
       </Zone>
---
+++ src/use-boolean/index.ts
@@ -0,0 +1,44 @@
+import { useCreation } from '../use-creation'
+import { useToggle } from '../use-toggle'
+
+import type { ReactSetState } from '../use-safe-state'
+
+export type UseBooleanActions = {
+  /**
+   * Set the boolean state to `true`.
+   */
+  setTrue: () => void
+  /**
+   * Set the boolean state to `false`.
+   */
+  setFalse: () => void
+  /**
+   * Set the boolean state to the given value.
+   */
+  setState: ReactSetState<boolean>
+  /**
+   * Toggle the boolean state.
+   */
+  toggle: () => void
+}
+
+export type UseBooleanReturns = readonly [value: boolean, UseBooleanActions]
+
+/**
+ * A hook to manage a boolean state.
+ *
+ * @param {boolean} [initialValue=true] The initial value of the boolean state.
+ *
+ */
+export function useBoolean(initialValue: boolean = true): UseBooleanReturns {
+  const [value, toggle, _actions] = useToggle(initialValue)
+
+  const actions = useCreation(() => ({
+    setTrue: () => _actions.setState(true),
+    setFalse: () => _actions.setState(false),
+    setState: _actions.setState,
+    toggle,
+  }))
+
+  return [value, actions] as const
+}
