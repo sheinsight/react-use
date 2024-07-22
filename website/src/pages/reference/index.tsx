22f789de5baaedbf52617642a0aed3444f91e9d8
Viki <hi@viki.moe>
2024-07-22 17:33:40 +0800

    docs: modify default reference route

--- website/src/pages/reference/index.tsx
+++ website/src/pages/reference/index.tsx
@@ -4,6 +4,6 @@ import hooks from '../components/search/hooks.json'
 
 export default function Reference() {
   const { siteConfig } = useDocusaurusContext()
-  const hook = hooks[0]?.name ?? 'create-single-loading'
+  const hook = hooks.find((e) => e.category === 'State')?.name ?? 'useBoolean'
   return <Redirect to={`${siteConfig.baseUrl}reference/${hook}`} />
 }
