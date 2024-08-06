31a59ca6ad01bae045e9e22e04c518ace4d4c3f4
Viki <hi@viki.moe>
2024-08-06 10:37:41 +0800

    perf(useLoadingSlowFn):  using getter to refine performance

--- src/use-loading-slow-fn/index.ts
+++ src/use-loading-slow-fn/index.ts
@@ -4,6 +4,7 @@ import { useLatest } from '../use-latest'
 import { useTimeout } from '../use-timeout'
 
 import type { UseAsyncFnOptions, UseAsyncFnReturns } from '../use-async-fn'
+import { useRender } from '../use-render'
 import type { AnyFunc } from '../utils/basic'
 
 export interface UseLoadingSlowFnOptions extends UseAsyncFnOptions {
@@ -31,29 +32,50 @@ export interface UseLoadingSlowFnReturns<T extends AnyFunc> extends UseAsyncFnRe
 export function useLoadingSlowFn<T extends AnyFunc>(fn: T, options: UseLoadingSlowFnOptions = {}) {
   const { loadingTimeout = 0, onLoadingSlow, ...useAsyncFnOptions } = options
 
-  const versionRef = useRef(0)
-  const latest = useLatest({ fn })
+  const render = useRender()
+  const latest = useLatest({ fn, loadingTimeout })
 
-  const { isTimeout, ...controls } = useTimeout(loadingTimeout, {
-    controls: true,
-    callback: onLoadingSlow,
+  const stateRef = useRef({
+    version: 0,
+    timer: undefined as ReturnType<typeof setTimeout> | undefined,
+    loadingSlow: { used: false, value: false },
   })
+
+  function updateRefValue<T>(refItem: { used: boolean; value: T }, newValue: T, update = true) {
+    if (refItem.value === newValue) return
+    refItem.value = newValue
+    refItem.used && update && render()
+  }
+
+  function runWhenVersionMatch(version: number, fu: AnyFunc) {
+    version === stateRef.current.version && fu()
+  }
 
   const asyncFn = useAsyncFn(
     (async () => {
-      if (controls.isActive()) {
-        controls.pause()
+      updateRefValue(stateRef.current.loadingSlow, false)
+
+      const version = ++stateRef.current.version
+
+      if (latest.current.loadingTimeout) {
+        stateRef.current.timer = setTimeout(() => {
+          runWhenVersionMatch(version, () => {
+            stateRef.current.timer = undefined
+            updateRefValue(stateRef.current.loadingSlow, true)
+            onLoadingSlow?.()
+          })
+        }, latest.current.loadingTimeout)
       }
 
-      controls.reset()
-      controls.resume()
-
-      const ver = ++versionRef.current
       const result = await latest.current.fn()
 
-      if (ver === versionRef.current) {
-        controls.reset()
+      if (stateRef.current.timer) {
+        clearTimeout(stateRef.current.timer)
       }
+
+      runWhenVersionMatch(version, () => {
+        updateRefValue(stateRef.current.loadingSlow, false)
+      })
 
       return result
     }) as T,
@@ -61,7 +83,21 @@ export function useLoadingSlowFn<T extends AnyFunc>(fn: T, options: UseLoadingSl
   )
 
   return {
-    ...asyncFn,
-    loadingSlow: isTimeout,
+    mutate: asyncFn.mutate,
+    run: asyncFn.run,
+    cancel: asyncFn.cancel,
+    get value() {
+      return asyncFn.value
+    },
+    get error() {
+      return asyncFn.error
+    },
+    get loading() {
+      return asyncFn.loading
+    },
+    get loadingSlow() {
+      stateRef.current.loadingSlow.used = true
+      return stateRef.current.loadingSlow.value
+    },
   }
 }
