144172ab466409efccda13add3a6dc17cce5012e
Viki <hi@viki.moe>
2024-08-14 16:13:23 +0800

    chore: add todo

--- src/use-supported/index.ts
+++ src/use-supported/index.ts
@@ -6,6 +6,8 @@ import type { DependencyList } from 'react'
 
 /**
  * A React Hook that helps to check if browser supports a feature in both client and Server-side Rendering (SSR).
+ *
+ * TODO: in 2.0 use Ref to track & mark dependencies to reduce render, only update when deps actually used and changed.
  */
 export function useSupported(callback: () => unknown, deps: DependencyList = []): boolean {
   const [isSupported, setIsSupported] = useSafeState<boolean>(false)
