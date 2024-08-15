19b1147ff70dd282753e64ed210088022b3c3213
Viki <hi@viki.moe>
2024-08-15 17:10:15 +0800

    chore: modify docs scripts

--- docs/scripts/release-documentation.ts
+++ docs/scripts/release-documentation.ts
@@ -3,3 +3,7 @@ import 'zx/globals'
 const commitHash = (await $`git rev-parse --short HEAD`).stdout.replace(/(\n|,$)/g, '').trim()
 
 await $`git tag doc-release/1.0.0/${commitHash}`
+
+await $`git push gitlab main --tags`
+
+await $`git tag -d doc-release/1.0.0/${commitHash}`
