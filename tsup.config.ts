f0d439961acb43dd1f626864da4bfd71fd1674ac
Viki <hi@viki.moe>
2024-08-23 10:49:55 +0800

    chore: update tsconfig config, fix reference issues

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   target: 'es5',
   entry: ['./src/index.ts'],
+  tsconfig: "./tsconfig.build.json",
   dts: true,
   clean: true,
   splitting: true,
