03915a25809cc4befe13b740fb1393d98f284b87
Viki <hi@viki.moe>
2024-10-24 15:05:39 +0800

    test: add more test cases

--- packages/react-use/src/use-element-bounding/index.test.ts
+++ packages/react-use/src/use-element-bounding/index.test.ts
@@ -1,8 +1,52 @@
-import { describe, expect, it } from 'vitest'
+import { renderHook } from '@/test'
+import { afterEach, beforeEach, describe, expect, it } from 'vitest'
 import { useElementBounding } from './index'
 
 describe('useElementBounding', () => {
+  let div: HTMLDivElement
+
+  beforeEach(() => {
+    div = document.createElement('div')
+    div.style.width = '200px'
+    div.style.height = '100px'
+    document.body.appendChild(div)
+  })
+
+  afterEach(() => {
+    div.remove()
+  })
+
   it('should defined', () => {
     expect(useElementBounding).toBeDefined()
   })
+
+  it('should return initial value', () => {
+    const { result } = renderHook(() => useElementBounding(null))
+
+    expect(result.current[0].x).toBe(0)
+    expect(result.current[0].y).toBe(0)
+    expect(result.current[0].width).toBe(0)
+    expect(result.current[0].height).toBe(0)
+    expect(result.current[0].left).toBe(0)
+    expect(result.current[0].top).toBe(0)
+    expect(result.current[0].right).toBe(0)
+    expect(result.current[0].bottom).toBe(0)
+    expect(result.current[1]).toBeInstanceOf(Function)
+  })
+
+  // for now, jsdom/happy-dom not support `MutationObserver`
+
+  // it('should return bounding value', () => {
+  //   const { result } = renderHook(() => useElementBounding(div))
+
+  //   expect(result.current[0].x).toBe(0)
+  //   expect(result.current[0].y).toBe(0)
+  //   expect(result.current[0].width).toBe(0)
+  //   expect(result.current[0].height).toBe(0)
+  //   expect(result.current[0].left).toBe(0)
+  //   expect(result.current[0].top).toBe(0)
+  //   expect(result.current[0].right).toBe(0)
+  //   expect(result.current[0].bottom).toBe(0)
+  //   expect(result.current[1]).toBeInstanceOf(Function)
+  // })
 })
--- packages/react-use/src/use-element-by-point/index.test.ts
+++ packages/react-use/src/use-element-by-point/index.test.ts
@@ -1,8 +1,15 @@
+import { renderHook } from '@/test'
 import { describe, expect, it } from 'vitest'
 import { useElementByPoint } from './index'
 
 describe('useElementByPoint', () => {
   it('should defined', () => {
     expect(useElementByPoint).toBeDefined()
+  })
+
+  it('should return initial value', () => {
+    const { result } = renderHook(() => useElementByPoint({ x: 0, y: 0 }))
+    expect(result.current.element).toBe(null)
+    expect(result.current.isSupported).toBe(false)
   })
 })
