f2f7b4371d1226f343d3e6795da886dcd26673e3
Viki <hi@viki.moe>
2024-10-30 19:50:06 +0800

    test: add more test cases

--- packages/react-use/src/use-key-stroke-once/index.test.ts
+++ packages/react-use/src/use-key-stroke-once/index.test.ts
@@ -1,8 +1,31 @@
-import { describe, expect, it } from 'vitest'
+import { act, renderHook } from '@/test'
+import { describe, expect, it, vi } from 'vitest'
 import { useKeyStrokeOnce } from './index'
 
 describe('useKeyStrokeOnce', () => {
   it('should defined', () => {
     expect(useKeyStrokeOnce).toBeDefined()
+  })
+
+  it('should return initial value', () => {
+    const callback = vi.fn()
+
+    renderHook(() => useKeyStrokeOnce('a', callback))
+
+    expect(callback).toHaveBeenCalledTimes(0)
+
+    act(() => {
+      const event = new KeyboardEvent('keydown', { key: 'a' })
+      window.dispatchEvent(event)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1)
+
+    act(() => {
+      const event = new KeyboardEvent('keydown', { key: 'a' })
+      window.dispatchEvent(event)
+    })
+
+    expect(callback).toHaveBeenCalledTimes(1) // should not be called again
   })
 })
