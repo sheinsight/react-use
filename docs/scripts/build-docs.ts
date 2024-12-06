3afb4ce2df30d0bfb7e4deb3ad17e9c2f0fa5986
Viki <hi@viki.moe>
2024-12-06 15:56:55 +0800

    docs: fix docs build

--- docs/scripts/build-docs.ts
+++ docs/scripts/build-docs.ts
@@ -1,16 +1,15 @@
 import 'zx/globals'
 
 const isSoDoc = process.env.PIPLINENAME === 'SODOC'
-const __dirname = new URL('.', import.meta.url).pathname
 
 if (isSoDoc) {
   process.env.IS_SODOC = 'true'
 
-  await $`pnpm add @alita/rspress-plugin@^1.5.0-beta.2 @shein/rspress-plugin-sodoc`
+  await $`pnpm add @alita/rspress-plugin@^1.6.3 @shein/rspress-plugin-sodoc`
 }
 
 await $`rspress build`
 
 if (isSoDoc) {
-  await $({ cwd: __dirname })`cp -r ../docs-dist ../../docs-dist`
+  await $`cp -r ./docs-dist ../docs-dist`
 }
