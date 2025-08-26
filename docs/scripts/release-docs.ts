8edf8b59978c2d04da5da23dd240bbefdbcef603
Viki <hi@viki.moe>
2025-08-26 15:01:36 +0800

    docs: update doc release scripts

--- docs/scripts/release-docs.ts
+++ docs/scripts/release-docs.ts
@@ -1,11 +1,8 @@
 import 'zx/globals'
 
-$.verbose = true
-
 const commitHash = (await $`git rev-parse --short HEAD`).stdout.replace(/(\n|,$)/g, '').trim()
+const latestTag = (await $`git describe --tags --abbrev=0`).stdout.replace(/(\n|,$)/g, '').trim()
 
-await $`git tag doc-release/1.0.0/${commitHash}`
-
+await $`git tag doc-release/${latestTag}/${commitHash}`
 await $`git push gitlab main --tags`
-
-await $`git tag -d doc-release/1.0.0/${commitHash}`
+await $`git tag -d doc-release/${latestTag}/${commitHash}`
