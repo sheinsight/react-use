394be263ae80eb30acf652e6de694906f3e6f94c
Viki <hi@viki.moe>
2025-10-28 11:16:21 +0800

    chore: 优化 CI 发布脚本

---
+++ scripts/publish-ci.ts
@@ -0,0 +1,9 @@
+import { execSync } from 'node:child_process'
+import { version } from '../package.json'
+
+let command = 'pnpm publish -r --provenance --no-git-checks --access public'
+
+if (version.includes('beta')) command += ' --tag beta'
+if (version.includes('alpha')) command += ' --tag alpha'
+
+execSync(command, { stdio: 'inherit' })
