ffb2ae53945dc7eab3de18ed754ef54c13363682
Viki <hi@viki.moe>
2025-11-17 15:04:01 +0800

    fix(useElementSize): 修复元素在渲染后卸载重新挂载场景尺寸获取异常的问题

--- packages/react-use/src/use-element-size/demo.tsx
+++ packages/react-use/src/use-element-size/demo.tsx
@@ -1,18 +1,25 @@
-import { Card } from '@/components'
-import { useElementSize } from '@shined/react-use'
+import { Button, Card, CodeBlock } from '@/components'
+import { useElementSize, useToggle } from '@shined/react-use'
 
 export function App() {
+  const [show, { toggle }] = useToggle(true)
   const { height, width } = useElementSize('#el-size')
 
   return (
     <Card>
-      <textarea
-        id="el-size"
-        disabled
-        className="input-border w-80 h-40 rounded resize text-lg"
-        value={JSON.stringify({ height, width }, null, 2)}
-        onChange={() => {}} // Prevent React warning
-      />
+      <Button onClick={toggle}>切换</Button>
+
+      <CodeBlock lang="json">{JSON.stringify({ height, width }, null, 2)}</CodeBlock>
+
+      {show && (
+        <textarea
+          id="el-size"
+          disabled
+          className="input-border w-80 h-40 rounded resize text-lg"
+          defaultValue="try to resize the textarea to test this hook"
+          onChange={() => {}} // Prevent React warning
+        />
+      )}
     </Card>
   )
 }
