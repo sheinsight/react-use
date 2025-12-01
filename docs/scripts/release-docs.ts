08bc3d11cddd4871baf1e0bea6e912a7b7bd2fb7
Viki <hi@viki.moe>
2025-12-01 16:58:57 +0800

    docs: lock goober to 2.1.16

--- docs/scripts/release-docs.ts
+++ docs/scripts/release-docs.ts
@@ -1,8 +1,8 @@
 import 'zx/globals'
 
 const commitHash = (await $`git rev-parse --short HEAD`).stdout.replace(/(\n|,$)/g, '').trim()
-const pkgVersion = (await $`node -p "require('./package.json').version"`).stdout.replace(/(\n|,$)/g, '').trim()
+const latestTag = (await $`git describe --tags --abbrev=0`).stdout.replace(/(\n|,$)/g, '').trim()
 
-await $`git tag doc-release/${pkgVersion}/${commitHash}`
+await $`git tag doc-release/${latestTag}/${commitHash}`
 await $`git push gitlab main --tags`
-await $`git tag -d doc-release/${pkgVersion}/${commitHash}`
+await $`git tag -d doc-release/${latestTag}/${commitHash}`
