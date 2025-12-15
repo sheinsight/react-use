89aa212ba535899189efc8a65b7c324edeed5c18
Viki <hi@viki.moe>
2025-12-15 17:21:55 +0800

    chore: 移除 NPM TOKEN 环境变量 & 优化构建脚本

--- scripts/publish-ci.ts
+++ scripts/publish-ci.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'node:child_process'
 import { version } from '../package.json'
 
-let command = 'pnpm publish -r --provenance --no-git-checks --access public'
+let command = 'pnpm publish -r --no-git-checks --access public'
 
 if (version.includes('beta')) command += ' --tag beta'
 if (version.includes('alpha')) command += ' --tag alpha'
